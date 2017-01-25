5884055bca7f25b0f214f117fd5da36505a2ecc6
Hanns Holger Rutz <contact@sciss.de>
2017-01-25 01:07:18 +0100

    ok, we still have a problem with 1D read through: `sampleRange` will be called with illegal index -1 when streamDim == -1.

--- core/src/main/scala/de/sciss/lucre/matrix/impl/ReaderImpl.scala
+++ core/src/main/scala/de/sciss/lucre/matrix/impl/ReaderImpl.scala
@@ -35,15 +35,15 @@ trait ReaderImpl extends Matrix.Reader {
 
   private[this] val numFramesI: Int = if (streamDim < 0) 1 else section(streamDim).size
 
-  final val numChannels: Int = {
-    val size = (1L /: section) ((prod, r) => prod * r.size)
-    val numChannelsL = size / numFramesI
+  final val size: Long = if (section.isEmpty) 0L else (1L /: section)(_ * _.size)
+
+  private[this] val numChannelsL: Long = size / numFramesI
+
+  final def numChannels: Int = {
     if (numChannelsL > 0xFFFF)
       throw new UnsupportedOperationException(s"The number of channels ($numChannelsL) is larger than supported")
     numChannelsL.toInt
   }
-
-  final val size: Long = if (section.isEmpty) 0L else (1L /: section)(_ * _.size)
 
   private[this] var pos = 0L
 
@@ -64,9 +64,10 @@ trait ReaderImpl extends Matrix.Reader {
 
     var i = off
     val j = off + len
+    val numCh = numChannels
     while (i < j) {
       var ch = 0
-      while (ch < numChannels) {
+      while (ch < numCh) {
         fBuf(ch)(i) = indexMap.nextFloat(it)
         ch += 1
       }
@@ -79,8 +80,8 @@ trait ReaderImpl extends Matrix.Reader {
   final def readDouble1D(dBuf: Array[Double], off: Int, len: Int): Unit = {
     if (len < 0) throw new IllegalArgumentException(s"Illegal read length $len")
     val stop = pos + len
-    if (stop > numFramesI) throw new EOFException(s"Reading past the end ($stop > $numFramesI)")
-    val sect1 = if (pos == 0 && stop == numFramesI) section else {
+    if (stop > size) throw new EOFException(s"Reading past the end ($stop > $size)")
+    val sect1 = if (pos == 0 && stop == size) section else {
       val newRange = sampleRange(section(streamDim), pos, stop)
       section.updated(streamDim, newRange)
     }
